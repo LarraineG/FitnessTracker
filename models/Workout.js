00e713de720182e310920b74142fa88872487f89
Larraine Gonzales <larrainegonzales@hotmail.com>
2020-09-29 20:51:32 -0500

    missing file, edited as needed

---
+++ models/Workout.js
@@ -0,0 +1,48 @@
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema ({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises : [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+            name: {
+                type: String,
+                trim: true,
+            },
+            distance: {
+                type: Number
+            },
+            duration: {
+                type: Number,
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            duration: {
+                type: Number
+            }
+        }
+    ]
+},
+{
+    timestamps: true
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
